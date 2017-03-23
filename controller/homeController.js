c375637a26359f1aa6859bc3e1b3ccdde8226ef3
Yann Carlen <yann.carlen@epitech.eu>
2017-03-23 17:18:21 +0100

    login and repository list

--- controller/homeController.js
+++ controller/homeController.js
@@ -3,8 +3,13 @@
  */
 
 app.controller('homeController', ['$scope', 'localStorageService', 'blihService', function ($scope, localStorageService, blihService) {
-    const user = localStorageService.get('user')
+    let user = localStorageService.get('user')
     const modal = $('#loginModal')
+    $scope.logError = false
+    $scope.userData = {
+        userName: '',
+        password: ''
+    }
 
     if (!user) {
         modal.modal({ backdrop: 'static', keyboard: false })
@@ -12,15 +17,33 @@ app.controller('homeController', ['$scope', 'localStorageService', 'blihService'
     }
 
     $scope.submitForm = () => {
+        if ($scope.logError &&
+            $scope.userData.userName == $scope.user.email &&
+            $scope.userData.password == $scope.user.password) return
         if ($scope.userForm.$valid) {
-            const userName = $scope.user.email;
+            const userName = $scope.user.email
             const token = blihService.getToken($scope.user.password)
+            $scope.userData.userName = userName
+            $scope.userData.password = $scope.user.password
             blihService.getRepositoryList(userName, token)
                 .then(list => {
+                    const repositoryList = list.split('\n')
+                    user = {
+                        userName,
+                        token,
+                        repositoryList,
+                        log: true
+                    }
+                    localStorageService.set('user', user)
                     modal.modal('hide')
+                    $scope.userData = null
+                    $scope.user = user
+                    $scope.logError = false
+                    $scope.$apply()
                 })
-                .catch(err => {
-
+                .catch(_ => {
+                    $scope.logError = true
+                    $scope.$apply()
                 })
         }
     }
