28fd1c82b305d7192c9ebdf0ec952da226289b7b
Yann Carlen <yann.carlen@epitech.eu>
2017-04-03 23:07:19 +0200

    begin delete repo

--- controller/listController.js
+++ controller/listController.js
@@ -5,8 +5,23 @@
 app.controller('listController', ['$scope', 'localStorageService', function ($scope, localStorageService) {
     const user = localStorageService.get('user')
     $scope.repositoryList = user.repositoryList
+    const deleteModal = $('#deleteModal')
 
     if (!user) {
         //TODO(carlen) redirect to home
+    }
+
+    $scope.deleteEvent = name => {
+        $scope.deleteRepoName = name
+        deleteModal.modal('show')
+    }
+
+    $scope.closeModalEvent = () => {
+        $scope.deleteRepoName = ''
+        deleteModal.modal('hide')
+    }
+
+    $scope.deleteRepoEvent = () => {
+
     }
 }])
